898a950f73248c1f40cf4116f86d79cb6d52de78
Catherine Hilton <126924004+cat-thebeesknees@users.noreply.github.com>
2023-07-28 11:49:10 -0400

    tab two complete. Added Routes tag and assigned paths to the Route tag.

--- React-Router/src/App.jsx
+++ React-Router/src/App.jsx
@@ -1,19 +1,24 @@
-import { useState } from 'react'
-
-import './App.css'
+import { useState } from "react";
+import "./App.css";
+import { Routes, Route } from "react-router-dom";
 
 function App() {
-  const [] = useState()
+  const [] = useState();
 
   return (
     <>
-     <div id="container">
-      <h1>Hello React Router!</h1>
-      <div id="navbar">{/* navigation here */}</div>
-      <div id="main-section">{/* routes here */}</div>
-    </div> 
+      <div id="container">
+        <h1>Hello React Router!</h1>
+        <div id="navbar">{/* navigation here */}</div>
+        <div id="main-section">
+          <Routes>
+            <Route path="/blue" element={<h1>Blue</h1>} />
+            <Route path="/red" element={<h1>Red</h1>} />
+          </Routes>
+        </div>
+      </div>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
