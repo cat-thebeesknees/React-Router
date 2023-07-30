e600d19bfb215c3420fd7c209a90bc51edfca582
Catherine Hilton <126924004+cat-thebeesknees@users.noreply.github.com>
2023-07-29 22:25:08 -0400

    added tons of styles to my application.

--- React-Router/src/App.jsx
+++ React-Router/src/App.jsx
@@ -1,29 +1,17 @@
 import "./App.css";
-import { Routes, Route, Link } from "react-router-dom";
-import Blue from "./components/Blue";
-import Red from "./components/Red";
-import Home from "./components/Home";
+import Navbar from "./components/Navbar";
+import MainSection from "./components/MainSection";
+
 
 function App() {
-  
-
   return (
     <>
-      <div id="container">
-        <h1>Hello React Router!</h1>
-        <div id="navbar">
-          <Link to="/blue">Blue</Link>
-          <Link to="/red">Red</Link>
-          <Link to="/">Home</Link>
+      
+        <div id="container">
+          <Navbar />
+          <MainSection />
         </div>
-        <div id="main-section">
-          <Routes>
-            <Route path="/blue" element={ Blue } />
-            <Route path="/red" element={ Red } />
-            <Route path="/" element={ Home } />
-          </Routes>
-        </div>
-      </div>
+      
     </>
   );
 }
